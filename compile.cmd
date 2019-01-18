12047a5ed4db108cdedb7dcb69d07bc42bee7e1d
User <dhkim0800@users.noreply.github.com>
2019-01-19 01:21:38 +0900

    Use 7za to build resource pack

--- compile.cmd
+++ compile.cmd
@@ -1,6 +1,9 @@
 echo off
 cls
-set /P "VER=Output folder name:"
+set /P "VER=Output version:"
 md %VER%
 xcopy /E packFormat %VER%
 copy ko_kr.json %VER%\assets\minecraft\lang
+if exist %VER%.zip del %VER%.zip
+7za a -sdel %VER%.zip .\%VER%\*
+rd /q /s %VER%
